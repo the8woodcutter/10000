a99a7ec3929edc8e47dc3f6b8c69209ddcb5fc7d
The WoodCutter <chunk@opoz.lan>
2024-11-11 17:11:45 -0700

    INIT #2

---
+++ bot.py
@@ -0,0 +1,15 @@
+import slixmpp
+import re, os, time, shelve
+
+class bot_game():
+	# this class will define the static parameters and functions of this game
+	pass
+
+class bot_connect():
+	# this class will define the function of this entire program and it's relation to slixmpp
+	pass
+
+class bot_run():
+	# this class will define the flow and control of bot_game() in order to play 10,000 dice game
+	pass
+
